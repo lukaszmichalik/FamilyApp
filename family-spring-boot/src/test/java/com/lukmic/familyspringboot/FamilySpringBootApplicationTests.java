cb8dcd5cfb224cea7fb72acd3e72bef38c807f27
lukasz.michalik <lukaszmichalik98@gmail.com>
2023-01-23 19:25:45 +0100

    Project init.

---
+++ family-spring-boot/src/test/java/com/lukmic/familyspringboot/FamilySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukmic.familyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
